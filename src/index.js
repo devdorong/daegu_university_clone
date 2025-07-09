c27044a9e45bd0f117a45b2a2f4abb3e88379d4e
devdorong <dev.dorong@gmail.com>
2025-07-09 12:04:22 +0900

    [docs] 기본구조

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import IndexPage from "./pages/IndexPage";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <div className="wrap">
+    <IndexPage></IndexPage>
+  </div>,
+);
---
+++ src/pages/IndexPage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "../components/haeder/Header";
+import Main from "../components/main/Main";
+import Footer from "../components/footer/Footer";
+
+function IndexPage() {
+  return (
+    <>
+      <Header></Header>
+      <Main></Main>
+      <Footer></Footer>
+    </>
+  );
+}
+
+export default IndexPage;
